d2428fa7334b53e14b62bdc026899709b598a9de
makashish <makanindian@gmail.com>
2025-08-19 10:38:49 +0530

    Updated gitignore, vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: "/quizexamfrontend/",
-})
+export default defineConfig(({ mode }) => ({
+    base: mode === 'github' ? '/quizexamfrontend/' : './',
+    plugins: [react()]
+}))
